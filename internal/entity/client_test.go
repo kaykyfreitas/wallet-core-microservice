505497ec64c1a2378f4f50c2fdc68f4178eacc8f
Kayky Freitas <kayky_defreitas@hotmail.com>
2024-06-16 17:50:01 -0300

    feat(account): add entity account

--- internal/entity/client_test.go
+++ internal/entity/client_test.go
@@ -37,3 +37,11 @@ func TestUpdateClientWithInvalidArgs(t *testing.T) {
 	err := client.Update("", "john@email.com")
 	assert.Error(t, err, "name is required")
 }
+
+func TestAddAccountToClient(t *testing.T) {
+	client, _ := NewClient("John Doe", "john@email.com")
+	account := NewAccount(client)
+	err := client.AddAccount(account)
+	assert.Nil(t, err)
+	assert.Equal(t, 1, len(client.Accounts))
+}
